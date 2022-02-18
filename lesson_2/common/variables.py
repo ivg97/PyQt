187bdd3a15cad3ac18b26508e8346ef88cc3e427
ivg97 <gavr97.97@mail.ru>
2022-02-18 20:11:02 +0300

    lesson 2

---
+++ lesson_2/common/variables.py
@@ -0,0 +1,32 @@
+import logging
+
+DEFAULT_PORT = 7779
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+LOGGING_LEVEL = logging.DEBUG
+
+# Прококол JIM основные ключи:
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'from'
+DESTINATION = 'to'
+
+# Прочие ключи, используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+EXIT = 'exit'
+
+
+RESPONSE_200 = {RESPONSE: 200}
+RESPONSE_400 = {
+            RESPONSE: 400,
+            ERROR: None
+        }
+
