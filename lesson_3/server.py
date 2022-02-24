406de638636e958d1483fcb686bcf5e4bff6a3c6
ivg97 <gavr97.97@mail.ru>
2022-02-24 16:10:32 +0300

    dz 3

---
+++ lesson_3/server.py
@@ -0,0 +1,203 @@
+import socket
+import sys
+import argparse
+import json
+import logging
+import select
+import time
+import threading
+import logs.config_server_log
+from errors import IncorrectDataRecivedError
+from common.variables import *
+from common.utils import *
+from decos import log
+from descryptors import Port
+from metaclasses import ServerMaker
+from server_database import ServerStorage
+
+
+logger = logging.getLogger('server')
+
+@log
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+    return listen_address, listen_port
+
+
+class Server(threading.Thread, metaclass=ServerMaker):
+    port = Port()
+
+    def __init__(self, listen_address, listen_port, database):
+        self.addr = listen_address
+        self.port = listen_port
+        self.database = database
+        self.clients = []
+        self.messages = []
+        self.names = dict()
+
+        super().__init__()
+
+    def init_socket(self):
+        logger.info(
+            f'Запущен сервер, порт для подключений: {self.port} ,'
+            f'адрес с которого принимаются подключения: {self.addr}. '
+            f'Если адрес не указан, принимаются соединения с любых адресов.')
+
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.bind((self.addr, self.port))
+        transport.settimeout(0.5)
+
+        self.sock = transport
+        self.sock.listen()
+
+    def run(self):
+        self.init_socket()
+
+        while True:
+            try:
+                client, client_address = self.sock.accept()
+            except OSError:
+                pass
+            else:
+                logger.info(f'Установлено соедение с ПК {client_address}')
+                self.clients.append(client)
+
+            recv_data_lst = []
+            send_data_lst = []
+            err_lst = []
+            try:
+                if self.clients:
+                    recv_data_lst, send_data_lst, err_lst = select.select(
+                        self.clients, self.clients, [], 0)
+            except OSError:
+                pass
+
+
+            if recv_data_lst:
+                for client_with_message in recv_data_lst:
+                    try:
+                        self.process_client_message(get_message(
+                            client_with_message), client_with_message)
+                    except:
+                        logger.info(f'Клиент '
+                                    f'{client_with_message.getpeername()} '
+                                    f'отключился от сервера.')
+                        self.clients.remove(client_with_message)
+
+
+            for message in self.messages:
+                try:
+                    self.process_message(message, send_data_lst)
+                except:
+                    logger.info(f'Связь с клиентом с именем '
+                                f'{message[DESTINATION]} была потеряна')
+                    self.clients.remove(self.names[message[DESTINATION]])
+                    del self.names[message[DESTINATION]]
+            self.messages.clear()
+
+
+    def process_message(self, message, listen_socks):
+        if message[DESTINATION] in self.names and\
+                self.names[message[DESTINATION]] in listen_socks:
+            send_message(self.names[message[DESTINATION]], message)
+            logger.info(f'Отправлено сообщение пользователю '
+                        f'{message[DESTINATION]} от пользователя '
+                        f'{message[SENDER]}.')
+        elif message[DESTINATION] in self.names and \
+                self.names[message[DESTINATION]] not in listen_socks:
+            raise ConnectionError
+        else:
+            logger.error(
+                f'Пользователь {message[DESTINATION]} '
+                f'не зарегистрирован на сервере, отправка '
+                f'сообщения невозможна.')
+
+
+    def process_client_message(self, message, client):
+        logger.debug(f'Разбор сообщения от клиента : {message}')
+        if ACTION in message and message[ACTION] == PRESENCE and TIME in \
+                message and USER in message:
+
+            if message[USER][ACCOUNT_NAME] not in self.names.keys():
+                self.names[message[USER][ACCOUNT_NAME]] = client
+                client_ip, client_port = client.getpeername()
+                self.database.user_login(message[USER][ACCOUNT_NAME],
+                                         client_ip, client_port)
+                send_message(client, RESPONSE_200)
+            else:
+                response = RESPONSE_400
+                response[ERROR] = 'Имя пользователя уже занято.'
+                send_message(client, response)
+                self.clients.remove(client)
+                client.close()
+            return
+        elif ACTION in message and message[ACTION] == MESSAGE and \
+                DESTINATION in message and TIME in message \
+                and SENDER in message and MESSAGE_TEXT in message:
+            self.messages.append(message)
+            return
+
+        elif ACTION in message and message[ACTION] == EXIT and \
+                ACCOUNT_NAME in message:
+            self.database.user_logout(message[ACCOUNT_NAME])
+            self.clients.remove(self.names[message[ACCOUNT_NAME]])
+            self.names[message[ACCOUNT_NAME]].close()
+            del self.names[message[ACCOUNT_NAME]]
+            return
+
+        else:
+            response = RESPONSE_400
+            response[ERROR] = 'Запрос некорректен.'
+            send_message(client, response)
+            return
+
+
+def print_help():
+    print('Поддерживаемые комманды:')
+    print('users - список известных пользователей')
+    print('connected - список подключенных пользователей')
+    print('loghist - история входов пользователя')
+    print('exit - завершение работы сервера.')
+    print('help - вывод справки по поддерживаемым командам')
+
+
+def main():
+    listen_address, listen_port = arg_parser()
+    database = ServerStorage()
+    server = Server(listen_address, listen_port, database)
+    server.daemon = True
+    server.start()
+    print_help()
+
+
+    while True:
+        command = input('Введите комманду: ')
+        if command == 'help':
+            print_help()
+        elif command == 'exit':
+            break
+        elif command == 'users':
+            for user in sorted(database.users_list()):
+                print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+        elif command == 'connected':
+            for user in sorted(database.active_users_list()):
+                print(f'Пользователь {user[0]}, подключен: '
+                      f'{user[1]}:{user[2]}, время установки соединения: '
+                      f'{user[3]}')
+        elif command == 'loghist':
+            name = input('Введите имя пользователя для просмотра истории. '
+                         'Для вывода всей истории, просто нажмите Enter: ')
+            for user in sorted(database.login_history(name)):
+                print(f'Пользователь: {user[0]} время входа: {user[1]}. '
+                      f'Вход с: {user[2]}:{user[3]}')
+        else:
+            print('Команда не распознана.')
+
+
+if __name__ == '__main__':
+    main()
